346b22406d57fc4da62f7630f501d40fc424662e
Razin Idzuddin <razin99@gmail.com>
2021-06-02 21:55:43 +1000

    used vscode cli to attach to a running container

--- run.sh
+++ run.sh
@@ -2,8 +2,19 @@
 
 # docker exec -it dev /bin/sh
 
-docker start dev && \
-docker exec dev /bin/sh -c 'cd /cs2511 && \
+# Container name
+CONTAINER="dev"
+# In hex, used by vscode CLI
+CONTAINER_HEX="$(echo -n $CONTAINER | xxd -ps -c 200)"
+
+# Path inside container to attach vscode to
+REMOTE_PATH="/cs2511"
+
+# Start and pull all new changes from given remote path
+docker start $CONTAINER && \
+docker exec $CONTAINER /bin/sh -c "cd $REMOTE_PATH && \
     ls -R --directory --color=never */.git \
-        | sed "s/\/.git//" \
-        | xargs -P10 -I{} git -C {} pull'
+        | sed \"s/\/.git//\" \
+        | xargs -P10 -I{} git -C {} pull"
+
+code --folder-uri="vscode-remote://attached-container+$CONTAINER_HEX/$REMOTE_PATH"
